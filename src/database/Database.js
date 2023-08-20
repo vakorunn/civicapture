a76c775563c73c67f4e1ed215861dfd2578b65dc
vakorunn <gitEvakOrunn@gmail.com>
2023-08-20 19:46:35 -0300

    first commit

---
+++ src/database/Database.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcuEx41LE-P-4-mGcIRgi5LtgcIJHx_SA",
+  authDomain: "civicapture.firebaseapp.com",
+  projectId: "civicapture",
+  storageBucket: "civicapture.appspot.com",
+  messagingSenderId: "7380933476",
+  appId: "1:7380933476:web:ffdd1a2cd2b1e63e447871"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db ;
