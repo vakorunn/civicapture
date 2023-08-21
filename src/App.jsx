6fef78f5a7a46bd1f23ca424e4fd1728e8e9727e
vakorunn <gitEvakOrunn@gmail.com>
2023-08-21 15:02:47 -0300

    Se añaden nuevas funciones de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import db from './database/Database'
 import { collection, getDocs } from 'firebase/firestore'
+
+import UploadButton from './components/UploadButton/UploadButton'
 
 const App = () => {
   const [data, setData] = useState([])
@@ -9,26 +11,27 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const query = collection(db,'miembros')
+        const query = collection(db, 'miembros')
         const allQuery = await getDocs(query)
-        const document = allQuery.docs.map(doc => ({id: doc.id, ...doc.data()}))
+        const document = allQuery.docs.map(doc => ({ id: doc.id, ...doc.data() }))
         setData(document)
       } catch (error) {
         console.log('Error durante la consulta ' + error);
       }
     }
     fetchData()
-  },[])
+  }, [])
   return (
     <div>
       <h2>Mis datos de Firebase</h2>
       <ul>
         {
           data.map(item => (
-            <li key={item.id}>{item.firstName} - {item.lastName}</li>
+            <li key={item.id}>{item.firstName} - {item.lastName} - {item.address.city} - {item.address.state}</li>
           ))
         }
       </ul>
+      <UploadButton />
     </div>
   )
 }
