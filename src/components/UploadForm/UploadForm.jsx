9e7533112b41bfac6a62d677520ff4757d219b3b
vakorunn <gitEvakOrunn@gmail.com>
2023-08-27 17:40:41 -0300

    Se aplican nuevos estilos para el formulario

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import db from '../../database/Database';
 
+import './UploadForm.css'
 
 const UploadForm = () => {
     const [message, setMessage] = useState('');
@@ -72,7 +73,7 @@ const UploadForm = () => {
             setFormData(initialFormData);
             setTimeout(() => {
                 setMessage('')
-            },2000)
+            }, 2000)
         } catch (error) {
             console.error('Error al agregar el nuevo documento:', error);
             setMessage('Error al agregar el nuevo documento');
@@ -80,88 +81,94 @@ const UploadForm = () => {
     };
 
     return (
-        <div>
-            <h2>Agregar nuevo Miembro</h2>
+        <div className='upload-form-container'>
+            <h2 className='minor-tittle'>Alta de Fiscales</h2>
             <form>
-                <h3>Datos Personales</h3>
-                <br />
-                <label>DNI </label>
-                <input type="text" name='dni' value={FormData.dni} onChange={handleInputChange} />
-                <br />
+                <div className='data-box'>
+                    <h3 className='box-header'>Datos Personales</h3>
+                    <label className='upload-from__label'>DNI </label>
+                    <input className='upload-form__input' type="text" name='dni' value={FormData.dni} onChange={handleInputChange} />
+                    <br />
 
-                <label>Nombre/s </label>
-                <input type="text" name='firstName' value={FormData.firstName} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Nombre/s </label>
+                    <input className='upload-form__input' type="text" name='firstName' value={FormData.firstName} onChange={handleInputChange} />
+                    <br />
 
-                <label>Apellido/s </label>
-                <input type="text" name='lastName' value={FormData.lastName} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Apellido/s </label>
+                    <input className='upload-form__input' type="text" name='lastName' value={FormData.lastName} onChange={handleInputChange} />
+                    <br />
 
-                <label>Fecha Nacimiento </label>
-                <input type="text" name='birthday' value={FormData.birthday} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Fecha Nacimiento </label>
+                    <input className='upload-form__input' type="text" name='birthday' value={FormData.birthday} onChange={handleInputChange} />
+                    <br />
 
-                <label>Telefono </label>
-                <input type="text" name='phoneNumber' value={FormData.phoneNumber} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Telefono </label>
+                    <input className='upload-form__input' type="text" name='phoneNumber' value={FormData.phoneNumber} onChange={handleInputChange} />
+                    <br />
 
-                <label>Departamento </label>
-                <input type="text" name='city' value={FormData.city} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Departamento </label>
+                    <input className='upload-form__input' type="text" name='city' value={FormData.city} onChange={handleInputChange} />
+                    <br />
 
-                <label>Provincia </label>
-                <input type="text" name='state' value={FormData.state} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Provincia </label>
+                    <input className='upload-form__input' type="text" name='state' value={FormData.state} onChange={handleInputChange} />
+                    <br />
 
-                <label>Direccion </label>
-                <input type="text" name='streetAddress' value={FormData.streetAddress} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Direccion </label>
+                    <input className='upload-form__input' type="text" name='streetAddress' value={FormData.streetAddress} onChange={handleInputChange} />
+                    <br />
+                </div>
 
-                <h3>Datos de la Fiscalización</h3>
-                <br />
-                <label>Circuito </label>
-                <input type="text" name='circuit' value={FormData.circuit} onChange={handleInputChange} />
-                <br />
+                <div className="data-box">
+                    <h3 className='box-header'>Datos de la Fiscalización</h3>
+                    <label className='upload-from__label'>Circuito </label>
+                    <input className='upload-form__input' type="text" name='circuit' value={FormData.circuit} onChange={handleInputChange} />
+                    <br />
 
-                <label>Cargo </label>
-                <input type="text" name='charge' value={FormData.charge} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Cargo </label>
+                    <input className='upload-form__input' type="text" name='charge' value={FormData.charge} onChange={handleInputChange} />
+                    <br />
 
-                <label>Lugar a Fiscalizar </label>
-                <input type="text" name='placeOfInspection' value={FormData.placeOfInspection} onChange={handleInputChange} />
-                <br />
-                
-                <h3>Datos Padronales</h3>
-                <br />
-                <label>Establecimiento </label>
-                <input type="text" name='establishment' value={FormData.establishment} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Lugar a Fiscalizar </label>
+                    <input className='upload-form__input' type="text" name='placeOfInspection' value={FormData.placeOfInspection} onChange={handleInputChange} />
+                    <br />
+                </div>
 
-                <label>Dirección </label>
-                <input type="text" name='votingAddress' value={FormData.votingAddress} onChange={handleInputChange} />
-                <br />
+                <div className="data-box">
+                    <h3 className='box-header'>Datos Padronales</h3>
+                    <label className='upload-from__label'>Establecimiento </label>
+                    <input className='upload-form__input' type="text" name='establishment' value={FormData.establishment} onChange={handleInputChange} />
+                    <br />
 
-                <label>Districto </label>
-                <input type="text" name='district' value={FormData.district} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Dirección </label>
+                    <input className='upload-form__input' type="text" name='votingAddress' value={FormData.votingAddress} onChange={handleInputChange} />
+                    <br />
 
-                <label>Circuito Electoral </label>
-                <input type="text" name='electoralCircuit' value={FormData.electoralCircuit} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Districto </label>
+                    <input className='upload-form__input' type="text" name='district' value={FormData.district} onChange={handleInputChange} />
+                    <br />
 
-                <label>Seccion Electoral </label>
-                <input type="text" name='electoralSection' value={FormData.electoralSection} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Circuito Electoral </label>
+                    <input className='upload-form__input' type="text" name='electoralCircuit' value={FormData.electoralCircuit} onChange={handleInputChange} />
+                    <br />
 
-                <label>Mesa </label>
-                <input type="text" name='table' value={FormData.table} onChange={handleInputChange} />
-                <br />
+                    <label className='upload-from__label'>Seccion Electoral </label>
+                    <input className='upload-form__input' type="text" name='electoralSection' value={FormData.electoralSection} onChange={handleInputChange} />
+                    <br />
 
-                <label>Orden </label>
-                <input type="text" name='order' value={FormData.order} onChange={handleInputChange} />
-                <br /><br />
+                    <label className='upload-from__label'>Mesa </label>
+                    <input className='upload-form__input' type="text" name='table' value={FormData.table} onChange={handleInputChange} />
+                    <br />
+
+                    <label className='upload-from__label'>Orden </label>
+                    <input className='upload-form__input' type="text" name='order' value={FormData.order} onChange={handleInputChange} />
+                    <br /><br />
+                </div>
             </form>
-            <button onClick={addNewDocument}>Agregar Documento</button>
+            <button onClick={addNewDocument} className='animated-button'>
+                <span>Añadir Fisal</span>
+                <span></span>
+            </button>
             <p>{message}</p>
         </div>
     )
