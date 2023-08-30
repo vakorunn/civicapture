2c5165b8c7bc542ae8dbbee61be9b66680c11801
vakorunn <gitEvakOrunn@gmail.com>
2023-08-30 12:59:27 -0300

    Se modifica la forma de ver los datos, de lista pasa a ser un nuevo formato de tabla

--- src/components/ListMembers/ListMembers.jsx
+++ src/components/ListMembers/ListMembers.jsx
@@ -5,6 +5,9 @@ import './ListMembers.css'
 import db from '../../database/Database'
 import { collection, getDocs } from 'firebase/firestore'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import Table from 'react-bootstrap/Table';
 
 const ListMembers = () => {
     const [data, setData] = useState([])
@@ -25,16 +28,38 @@ const ListMembers = () => {
     return (
         <div className='members-list-container'>
             <h2 className='minor-tittle'>Listado de Fiscales</h2>
-            <ul>
-                {
-                    data.map(item => (
-                        <li key={item.id}>
-                            <Link to={`/listaFiscales/${item.dni}`} className='access' state={{memberData: item}}>{item.dni}</Link> | 
-                            {item.firstName} {item.lastName} | {item.birthday} | {item.ocupation.charge} | {item.ocupation.placeOfInspection} |
-                        </li>
-                    ))
-                }
-            </ul>
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>DNI</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Cargo</th>
+                        <th>Lugar a Fiscalizar</th>
+                        <th>Info</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map(item => (
+                            <tr key={item.id}>
+                                <td>{item.dni}</td>
+                                <td>{item.firstName}</td>
+                                <td>{item.lastName}</td>
+                                <td>{item.ocupation.charge}</td>
+                                <td>{item.ocupation.placeOfInspection}</td>
+                                <td>
+                                    <Link to={`/listaFiscales/${item.dni}`} state={{ memberData: item }}>
+                                        <button className='more-info'>
+                                            <FontAwesomeIcon icon={faEye} style={{ color: "#891cc4" }} />
+                                        </button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
         </div>
     )
 }
