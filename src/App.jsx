fb947cd383c18fc43e096c12bc1403c0d4748368
vakorunn <gitEvakOrunn@gmail.com>
2023-08-31 10:26:25 -0300

    Se añaden nuevas rutas para acceder a los nuevos listados

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import News from './components/News/News'
 import UploadForm from './components/UploadForm/UploadForm'
 import UpdateForm from './components/UpdateForm/UpdateForm'
 import MemberDetails from './components/MemberDetails/MemberDetails'
+import AttorneyGeneralList from './components/AttorneyGeneralList/AttorneyGeneralList'
+import TableProsecutorList from './components/TableProsecutorList/TableProsecutorList'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
@@ -20,6 +22,8 @@ const App = () => {
           <Route path='/añadirFiscal' element={<UploadForm />}></Route>
           <Route path='/editarFiscal' element={<UpdateForm />}></Route>
           <Route path='/listaFiscales/:dni' Component={MemberDetails}></Route>
+          <Route path='/fiscalesMesa' element={<TableProsecutorList />} />
+          <Route path='/fiscalesGenerales' element={<AttorneyGeneralList />} />
         </Routes>
       </BrowserRouter>
     </>
