0b0008d4ebc1c57d8640979b7010b850c642f152
vakorunn <gitEvakOrunn@gmail.com>
2023-08-31 10:27:22 -0300

    Añadimos dos nuevas opciones al menu para listar a los distintos tipos de fiscales

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,6 +18,12 @@ const Navbar = () => {
             <NavLink className='navbar-element__link' to='/listaFiscales'>Lista de Fiscales</NavLink>
           </li>
           <li className='navbar-element'>
+            <NavLink className='navbar-element__link' to='/fiscalesGenerales'>Fiscales Generales</NavLink>
+          </li>
+          <li className='navbar-element'>
+            <NavLink className='navbar-element__link' to='/fiscalesMesa'>Fiscales Mesa</NavLink>
+          </li>
+          <li className='navbar-element'>
             <NavLink className='navbar-element__link' to='/añadirFiscal'>Añadir Fiscal</NavLink>
           </li>
           <li className='navbar-element'>
