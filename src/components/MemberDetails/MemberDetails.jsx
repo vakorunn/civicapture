64d42d4c1028f788926b817ac2c30e850e7730c1
vakorunn <gitEvakOrunn@gmail.com>
2023-09-16 18:47:53 -0300

    Comienza la modificacion del componente encargado a mostrar en detalle la informacion del miembro

--- src/components/MemberDetails/MemberDetails.jsx
+++ src/components/MemberDetails/MemberDetails.jsx
@@ -18,10 +18,31 @@ const MemberDetails = () => {
           <div className="row p-2 m-auto">
             <div className="row p-3 m-auto">
               <div className='col-6 border border-primary d-flex justify-content-center'>
-                <p>Apellido y Nombre: {memberData.lastName} {memberData.firstName}</p>
+                <p className='m-auto p-2'>Apellido y Nombre: {memberData.lastName} {memberData.firstName}</p>
               </div>
               <div className='col-6 border border-primary d-flex justify-content-center'>
-                <p>DNI: {}</p>
+                <p className='m-auto p-2'>DNI: {memberData.dni}</p>
+              </div>
+            </div>
+            <div className="row p-3 m-auto">
+              <div className="col-6 border border-primary d-flex justify-content-center">
+                <p className='m-auto p-2'>Telefono: {memberData.phoneNumber}</p>
+              </div>
+              <div className='col-6 border border-primary d-flex justify-content-center'>
+                <p className="m-auto p-2">Fecha de Nacimiento: {memberData.birthday}</p>
+              </div>
+            </div>
+            <div className="row p-3 m-auto">
+              <div className='col-6 border border-primary d-flex justify-content-center'>
+                <p className="m-auto p-2">Provincia: {memberData.state}</p>
+              </div>
+              <div className='col-6 border border-primary d-flex justify-content-center'>
+                <p className="m-auto p-2">Ciudad: {memberData.city}</p>
+              </div>
+            </div>
+            <div className="row p-3 m-auto">
+              <div className="col-12 border border-primary d-flex justiy-content-center">
+                <p className="m-auto p-2">Calle: {memberData.streetAddress}</p>
               </div>
             </div>
           </div>
