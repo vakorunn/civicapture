9d841f9f35d941f7c29058e0cd93495fa4e26060
vakorunn <136173711+vakorunn@users.noreply.github.com>
2023-09-25 19:51:09 +0000

    Comienza el desarrollo del componente encargado de actualizar los datos de los vehiculos

--- src/components/UpdateVihicleForm/UpdateVihicleForm.jsx
+++ src/components/UpdateVihicleForm/UpdateVihicleForm.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import db from '../../database/Database'
+
+import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore'
+import MySwal from 'sweetalert2'
 
 const UpdateVihicleForm = () => {
+  const [patentToUpdate, setPatentToUpdate] = useState('');
+  const [docRefToUpdate, setDocRefToUpdate] = useState('')
+  const [vihicleData, setVihicleData] = useState(null)
+
+  const handlePatentChange = (event) => {
+    setPatentToUpdate = event.target.value
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setMemberData((prevData) => ({
+      ...prevData,
+      [name]: value.toUpperCase()
+    }))
+  }
+
+  const searchVihicle = async () => {
+    
+  }
+
   return (
-    <div>UpdateVihicleForm</div>
+    <>
+    </>
   )
 }
 
